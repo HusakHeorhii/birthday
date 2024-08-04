8b4c449e7a5aa59eb47bc047a7060973f37f95c1
Heorhii Husak <107207517+HusakHeorhii@users.noreply.github.com>
2024-08-04 20:22:15 +0100

    Update index.js
    Fix

--- index.js
+++ index.js
@@ -2,14 +2,4 @@ let x = 0;
 
 while (x<=100){
   document.getElementById("text").textContent = ":D";
-  document.getElementById("text").textContent = "-:D";
-  document.getElementById("text").textContent = "---:D";
-  document.getElementById("text").textContent = "-----:D";
-  document.getElementById("text").textContent = "------:D";
-  document.getElementById("text").textContent = "-------:D";
-  document.getElementById("text").textContent = "------:D";
-  document.getElementById("text").textContent = "-----:D";
-  document.getElementById("text").textContent = "---:D";
-  document.getElementById("text").textContent = "-:D";
-  document.getElementById("text").textContent = ":D";
 }
